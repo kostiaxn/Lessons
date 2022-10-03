28122cd3ac3726219fa5878ccfa23a4a76c48287
kostiaxn <kostia.xn@gmail.com>
2022-10-03 11:26:11 +0300

    GUIComponents

---
+++ src/GUIComponents/GUIComponents.java
@@ -0,0 +1,12 @@
+package GUIComponents;
+
+import javax.swing.*;
+
+public class GUIComponents {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Моя программа");
+        frame.setBounds(300,300,300,250);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
